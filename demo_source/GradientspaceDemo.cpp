c8d6c6dcc53cc17bf836fbfa5fa1cc67d48f148a
Ryan Schmidt <rms@rms80.com>
2025-04-22 14:21:55 -0400

    GradientspaceIO working now

--- demo_source/GradientspaceDemo.cpp
+++ demo_source/GradientspaceDemo.cpp
@@ -4,12 +4,14 @@
 #include <iostream>
 
 #include "Mesh/DenseMesh.h"
+#include "MeshIO/OBJReader.h"
 
 int main()
 {
     std::cout << "Hello World!\n";
 
     GS::DenseMesh DenseMesh;
-
+    GS::OBJFormatData OBJData;
+    bool bReadOK = GS::OBJReader::ReadOBJ("C:\\scratch\\bunny_open_200.obj", OBJData);
 }
 
